61be93fa5bedea55b57960493848620b9724372c
Julian Martinez <julian.martinez.s@outlook.com>
2021-05-31 10:28:10 -0400

    Initial Commit

---
+++ Credit Card Scanner/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Credit Card Scanner
+//
+//  Created by Julian Martinez on 5/31/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
